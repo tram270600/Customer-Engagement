f6f5e6c531d3dd43bb97f5f99e81d745d7e647db
Tram <ngnt@netcompany.com>
2023-04-20 23:26:56 +0200

    Search result section

--- src/components/HorizontalStepper/HorizontalStepper.js
+++ src/components/HorizontalStepper/HorizontalStepper.js
@@ -3,28 +3,38 @@ import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import styles from "./HorizontalStepper.module.scss";
+import classNames from "classnames";
 
 const steps = [
   "Input the information of package",
   "Result of route",
   "Payment",
-  "Confirmation"
+  "Confirmation",
 ];
 
-export default function HorizontalStepper({controlStep, onBackBtClick}) {
+export default function HorizontalStepper({ activeStep, onBackBtClick }) {
   return (
     <>
-    <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={controlStep} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-    </Box>
-    <button className={controlStep === 0 ? styles.noDisplayBtn : ''} onClick={() => onBackBtClick()}>Back</button>
+      <Box sx={{ width: "100%" }}>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+      <button
+        className={classNames(styles.backBtn, {
+          [styles.noDisplayBtn]: activeStep === 0,
+        })}
+        onClick={() => onBackBtClick()}
+      >
+        <ArrowBackIcon />
+        Back
+      </button>
     </>
   );
 }
---
+++ src/pages/SearchRouteResult.js
@@ -0,0 +1,53 @@
+import styles from "../styles/searchRouteResult.module.scss";
+import PrimaryButton from "../components/buttons/PrimaryButton";
+
+export default function SearchRouteResult() {
+  const columnData = [
+    "From - To",
+    "Estimate time",
+    "Period",
+    "Price",
+    "Action",
+  ];
+  const contentData = [
+    {
+      id: 1,
+      fromTo: "A - B",
+      estimateTime: "12 hours",
+      period: "02/14/2023 - 02/18/2023",
+      price: "23",
+    },
+    {
+      id: 2,
+      fromTo: "A - B",
+      estimateTime: "12 hours",
+      period: "02/14/2023 - 02/18/2023",
+      price: "23",
+    },
+  ];
+  return (
+    <>
+      <div className={styles.routeSectionHeader}>Search Results</div>
+      <div className={styles.tableColumnGroupHeader}>
+        {columnData.map((item) => {
+          return <div className={styles.tableColumnHeader}>{item}</div>;
+        })}
+      </div>
+      <div className={styles.tableData}>
+        {contentData.map((content) => {
+          return (
+            <div key={content.id} className={styles.tableColumnData}>
+              <div>{content.fromTo}</div>
+              <div>{content.estimateTime}</div>
+              <div>{content.period}</div>
+              <div>{content.price}</div>
+              <div>
+                <PrimaryButton name="Select"></PrimaryButton>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
